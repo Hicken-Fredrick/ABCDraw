ca30a645db84f42b90e042dd7280fec6cee53473
Alexander Burov <alex_bbb@mail.ru>
2019-02-18 11:45:39 +0300

    Initial empty projest setup

---
+++ desktop/src/com/group/abcdraw/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.group.abcdraw.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.group.abcdraw.ABCDrawGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ABCDrawGame(), config);
+	}
+}
