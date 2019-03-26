ab96801cbe97b710b811f7a004af8029a343b5b1
Alexander Burov <alex_bbb@mail.ru>
2019-03-26 13:24:54 +0300

    A-001-002-Coordinate-Transformation

--- core/src/com/group/abcdraw/model/Position.java
+++ core/src/com/group/abcdraw/model/Position.java
@@ -1,5 +1,7 @@
 package com.group.abcdraw.model;
 
+import java.util.Objects;
+
 public class Position {
     float x;
     float y;
@@ -15,5 +17,14 @@ public class Position {
 
     public float getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return Float.compare(position.x, x) == 0 &&
+            Float.compare(position.y, y) == 0;
     }
 }
